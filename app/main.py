0d80b4c8e288664372215bb8b1d4706ef8e06c98
Artem <gavr.artem.s@gmail.com>
2024-11-06 21:12:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
+# from statistics import pstdev
+
+
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors" : [format_linter_error(er) for er in errors],
+        "path" : file_path,
+        "status" : "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(path, er)
+        for path, er in linter_report.items()]
